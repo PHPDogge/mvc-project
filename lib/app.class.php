cf3c4f2af937953b84b492adf906fbebc70913ab
Dmitrenko Mykhailo <36new1@gmail.com>
2018-12-30 16:12:55 +0200

    add wiews

--- lib/app.class.php
+++ lib/app.class.php
@@ -19,9 +19,17 @@ class App
         // Call controllers method
         $controller_object = new $controller_class;
         if (method_exists($controller_object, $controller_method)) {
-            $result = $controller_object->$controller_method();
+            // controllers action may return a view path
+            $view_path = $controller_object->$controller_method();
+            $view_object = new View($controller_object->getData(), $view_path);
+            $content = $view_object->render();
         } else {
             throw new Exception('Method' . $controller_method . 'of class' . $controller_class . 'does not exists');
         }
+
+        $layout = self::$router->getRoute();
+        $layout_path = VIEWS_PATH . DS . $layout . '.html';
+        $layout_view_object = new View(compact('content'), $layout_path);
+        echo $layout_view_object->render();
     }
 }
