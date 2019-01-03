b7dd1bf40b5466126d7d43f0c5679c035d5930cd
Dmitrenko Mykhailo <36new1@gmail.com>
2019-01-04 00:59:58 +0200

    connect CDN Bootstrap

--- controllers/pages.controller.php
+++ controllers/pages.controller.php
@@ -4,7 +4,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        echo "Here will be a pages list";
+        $this->data['test_content'] = 'Here will be a pages list';
     }
 
     public function view()
@@ -13,7 +13,7 @@ class PagesController extends Controller
 
         if (isset($params[0])) {
             $alias = strtolower($params[0]);
-            echo "here will be page with '{$alias}' alias";
+            $this->data['content'] = "here will be page with '{$alias}' alias";
         }
     }
 }
